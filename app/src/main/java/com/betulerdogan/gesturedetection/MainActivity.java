f507349fd0d5e06c8127f63bfaab70de97f016e3
betulerdogan <betulerdo6an@gmail.com>
2019-03-28 23:38:29 +0300

    no message

--- app/src/main/java/com/betulerdogan/gesturedetection/MainActivity.java
+++ app/src/main/java/com/betulerdogan/gesturedetection/MainActivity.java
@@ -1,13 +1,18 @@
 package com.betulerdogan.gesturedetection;
 
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private GestureDetectorCompat gDetect;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
